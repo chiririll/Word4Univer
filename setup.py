321055e6f580bac1a61a903147f4ee8e02d8e7e6
Kirill Chizhov <sstive39@gmail.com>
2022-09-25 01:18:39 +0300

    update setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Word4Univer',
+    version='0.1.0',
+    packages=['Word4Univer'],
+    url='',
+    license='MIT',
+    author='chiririll',
+    author_email='sstive39@gmail.com',
+    description='',
+    requires=['Jinja2']
+)
