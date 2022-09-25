7af513d1e4455b5f46d8d56d09f91c3da2f93ed0
Kirill Chizhov <sstive39@gmail.com>
2022-09-25 21:55:24 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from os import path
 from setuptools import setup, find_packages
 
 
 setup(
     name='Word4Univer',
     version='0.1.0',
-    package_dir={'': path.dirname(__file__)},
-    packages=find_packages(path.dirname(__file__), exclude=["tests*"]),
+    package_dir={'': "."},
+    packages=find_packages(".", exclude=["tests*"]),
     url='',
     license='MIT',
     author='chiririll',
     author_email='sstive39@gmail.com',
     description='',
-    requires=['Jinja2']
+    install_requires=['Jinja2~=3.1.2']
 )
